87d6d7c4bbb647d22d39616425b796b6dfb376d2
Priyaranjanp <priyaranjanpradhan92@gmail.com>
2021-03-15 20:36:04 +0530

    Code changed

--- src/main/java/com/bootsecurity/api/SpringbootSecurityApplication.java
+++ src/main/java/com/bootsecurity/api/SpringbootSecurityApplication.java
@@ -2,8 +2,10 @@ package com.bootsecurity.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class SpringbootSecurityApplication {
 
 	public static void main(String[] args) {
